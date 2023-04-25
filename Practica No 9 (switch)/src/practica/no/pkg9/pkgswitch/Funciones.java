c27d6e6036761a1d769612af6a09ec0c973d8738
Diego5212 <124754726+robocay1252@users.noreply.github.com>
2023-04-25 12:02:10 -0600

    Add files via upload

---
+++ Practica No 9 (switch)/src/practica/no/pkg9/pkgswitch/Funciones.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica.no.pkg9.pkgswitch;
+
+/**
+ *
+ * @author SAC2-5
+ */
+public class Funciones {
+    
+}
---
+++ Practica No 9 (switch)/src/practica/no/pkg9/pkgswitch/PracticaNo9Switch.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica.no.pkg9.pkgswitch;
+
+/**
+ *
+ * @author SAC2-5
+ */
+public class PracticaNo9Switch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
